58a4aba1d7ece35d3be0685c94a57169ad972373
Ian Castleden <ian.castleden@gmail.com>
2018-03-29 05:01:42 +0800

    added bioj

---
+++ mlcode/bioj.py
@@ -0,0 +1,108 @@
+import requests
+from mlabc import Clean, Download, Generate
+
+
+ISSN = {
+    '1470-8728': 'Biochem. J.',
+    '0264-6021': 'Biochem. J.'
+}
+
+
+class BIOJ(Clean):
+
+    def __init__(self, root):
+        self.root = root
+        a = root.select('div.article.fulltext-view')
+        assert a, a
+        self.article = a[0]
+
+    def results(self):
+        secs = self.article.select('div.section.results')
+        if secs:
+            return secs[0]
+        for sec in self.article.select('div.section'):
+            h2 = sec.find('h2')
+            if h2:
+                txt = h2.text.lower().strip()
+                if txt in {'results', 'results and discussion'}:
+                    return sec
+
+        return None
+
+    def methods(self):
+        secs = self.article.select('div.section.materials-methods')
+        if secs:
+            return secs[0]
+        secs = self.article.select('div.section.methods')
+        if secs:
+            return secs[0]
+        for sec in self.article.select('div.section'):
+            h2 = sec.find('h2')
+            if h2:
+                txt = h2.text.lower().strip()
+                if txt in {'methods', 'experimental procedures', 'materials and methods',
+                           'material and methods'}:  # spelling!
+                    return sec
+
+        return None
+
+    def abstract(self):
+        secs = self.article.select('div.section.abstract')
+        return secs[0] if secs else None
+
+    def tostr(self, sec):
+        for a in sec.select('div.fig.pos-float'):
+            p = self.root.new_tag('p')
+            p.string = '[[FIGURE]]'
+            a.replace_with(p)
+        for a in sec.select('p a.xref-bibr'):
+            a.replace_with('CITATION')
+        txt = [self.SPACE.sub(' ', p.text) for p in sec.select('p')]
+        return txt
+
+
+class GenerateBIOJ(Generate):
+    def create_clean(self, soup, pmid):
+        return BIOJ(soup)
+
+
+def gen_bioj(issn):
+
+    gad = GenerateBIOJ(issn)
+    gad.run()
+
+
+def download_bioj(issn, sleep=5.0, mx=0):
+
+    class D(Download):
+        Referer = 'http://www.biochemj.org'
+
+        def get_response(self, paper, header):
+            resp = requests.get('http://doi.org/{}'.format(paper.doi), headers=header)
+            if not resp.url.endswith('.full'):
+                resp = requests.get(resp.url + '.full', headers=header)
+            return resp
+
+        def check_soup(self, paper, soup, resp):
+            a = soup.select('div.article.fulltext-view')
+            # if not a and year <= 2001:  # probably only a (scanned?) PDF version
+            #    xml = b'failed-only-pdf'
+            #     d = fdir
+            #    failed.add(pmid)
+            # else:
+            assert a and len(a) == 1, (paper.pmid, resp.url)
+
+    o = D(issn, sleep=sleep, mx=mx)
+    o.run()
+
+
+def html_bioj(issn):
+
+    b = GenerateBIOJ(issn)
+    print(b.tohtml())
+
+
+if __name__ == '__main__':
+    download_bioj(issn='1470-8728', sleep=10., mx=2)
+    # download_bioj(issn='0264-6021', sleep=10., mx=2)
+    # gen_bioj(issn='0264-6021')
