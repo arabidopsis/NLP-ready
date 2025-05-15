d10f839d29e6c74c4871374e694bd4df79a713dd
Ian Castleden <ian.castleden@uwa.edu.au>
2025-05-15 18:13:07 +0800

    add tags

--- nlpready/epmc/utils.py
+++ nlpready/epmc/utils.py
@@ -12,6 +12,122 @@ NS = {
 }
 NSRE = re.compile("^{([^}]+)}(.*)$")
 
+HTMLTAGS = {
+    "a",
+    "abbr",
+    "address",
+    "area",
+    "article",
+    "aside",
+    "audio",
+    "b",
+    "base",
+    "bdi",
+    "bdo",
+    "blockquote",
+    "body",
+    "br",
+    "button",
+    "canvas",
+    "caption",
+    "cite",
+    "code",
+    "col",
+    "colgroup",
+    "data",
+    "datalist",
+    "dd",
+    "del",
+    "details",
+    "dfn",
+    "dialog",
+    "div",
+    "dl",
+    "dt",
+    "em",
+    "embed",
+    "fieldset",
+    "figcaption",
+    "figure",
+    "footer",
+    "form",
+    "h1",
+    "h2",
+    "h3",
+    "h4",
+    "h5",
+    "h6",
+    "head",
+    "header",
+    "hgroup",
+    "hr",
+    "html",
+    "i",
+    "iframe",
+    "img",
+    "input",
+    "ins",
+    "kbd",
+    "label",
+    "legend",
+    "li",
+    "link",
+    "main",
+    "map",
+    "mark",
+    "math",
+    "menu",
+    "meta",
+    "meter",
+    "nav",
+    "noscript",
+    "object",
+    "ol",
+    "optgroup",
+    "option",
+    "output",
+    "p",
+    "picture",
+    "pre",
+    "progress",
+    "q",
+    "rp",
+    "rt",
+    "ruby",
+    "s",
+    "samp",
+    "script",
+    "search",
+    "section",
+    "select",
+    "slot",
+    "small",
+    "source",
+    "span",
+    "strong",
+    "style",
+    "sub",
+    "summary",
+    "sup",
+    "svg",
+    "table",
+    "tbody",
+    "td",
+    "template",
+    "textarea",
+    "tfoot",
+    "th",
+    "thead",
+    "time",
+    "title",
+    "tr",
+    "track",
+    "u",
+    "ul",
+    "var",
+    "video",
+    "wbr",
+}
 
 PMCTAGS = {
     "p": "p",
@@ -82,8 +198,13 @@ class Events:
 
                 else:
                     if tag not in self.TAGS:
-                        MISSING.add(tag)
-                    etag = self.TAGS.get(tag, "span")
+                        if tag in HTMLTAGS:
+                            etag = tag
+                        else:
+                            etag = "span"
+                            MISSING.add(tag)
+                    else:
+                        etag = self.TAGS[tag]
                     yield f'<{etag} class="{tag}">'
 
                 if elem.text:
@@ -103,7 +224,13 @@ class Events:
                 if hasattr(self, stag):
                     yield getattr(self, stag)(elem)
                 else:
-                    etag = self.TAGS.get(tag, "span")
+                    if tag not in self.TAGS:
+                        if tag in HTMLTAGS:
+                            etag = tag
+                        else:
+                            etag = "span"
+                    else:
+                        etag = self.TAGS[tag]
                     yield "</%s> " % etag  # [sic!] add space
                 if elem.tail:
                     yield elem.tail
